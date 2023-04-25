0473af97273be630b9e9de857d911d9df0cd9490
MIRAI <mariyamtahri7@gmail.com>
2023-04-25 11:31:23 -0700

    putch

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,11 @@
 #include "main.h"
-	/**
-	 * main - Entry point
-	 * description : 'prints _putchar to stdout'
-	 * Return: Always 0
-	 */
-	int main(void)
-	{
+/**
+* main - Entry point
+* description : 'prints _putchar to stdout'
+* Return: Always 0
+*/
+int main(void)
+{
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
