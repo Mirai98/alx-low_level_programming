8661c6027da203b848ee3f75e718f13e32b9bd90
MIRAI <mariyamtahri7@gmail.com>
2023-04-25 10:46:13 -0700

    0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,9 @@
 #include "main.h"
 	/**
-	 * main - print _putchar
-	 * is written by mbah
-	 * Return: 0
+	 * main - Entry point
+	 * description : 'prints _putchar to stdout'
+	 * Return: Always 0
 	 */
-
-
 	int main(void)
 	{
 	_putchar('_');
@@ -18,5 +16,4 @@
 	_putchar('r');
 	_putchar('\n');
 	return (0);
-	
 }
