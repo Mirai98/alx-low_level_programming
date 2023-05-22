56ab6305e410558a256ac08aafebebfa56377f8e
MIRAI <mariyamtahri7@gmail.com>
2023-05-22 15:48:01 -0700

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - struct dog
+ * @name: name of the dog
+ * @age : age of the dog
+ * @owner: the name of the owner of the dog
+ *
+ * Description: dog Description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
