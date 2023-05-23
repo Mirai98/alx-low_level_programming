4e61ee7171b5f2b83cfc9d1e64751bfe48289627
MIRAI <mariyamtahri7@gmail.com>
2023-05-22 17:13:12 -0700

    gg

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -60,22 +60,22 @@ char *_strcpy(char *dest, char *src)
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
-	int len1, len2;
+	int l1, l2;
 
-	len1 = _strlen(name);
-	len2 = _strlen(owner);
+	l1 = _strlen(name);
+	l2 = _strlen(owner);
 
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
 
-	dog->name = malloc(sizeof(char) * (len1 + 1));
+	dog->name = malloc(sizeof(char) * (l1 + 1));
 	if (dog->name == NULL)
 	{
 		free(dog);
 		return (NULL);
 	}
-	dog->owner = malloc(sizeof(char) * (len2 + 1));
+	dog->owner = malloc(sizeof(char) * (l2 + 1));
 	if (dog->owner == NULL)
 	{
 		free(dog);
