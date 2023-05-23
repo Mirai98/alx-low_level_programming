79846232cefc8bbb80ad2f5d10c44a9f48182622
MIRAI <mariyamtahri7@gmail.com>
2023-05-22 17:09:52 -0700

    r

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -56,34 +56,23 @@ char *_strcpy(char *dest, char *src)
  *
  * Return: The new struct dog, NULL otherwise
  */
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
+	int len1, len2;
 
-	if (name == NULL || age < 0 || owner == NULL)
-		return (NULL);
+	len1 = _strlen(name);
+	len2 = _strlen(owner);
 
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
-		return (NULL)
+		return (NULL);
 
-	dog->name = malloc(sizeof(char) * (_strlen(name) + 1));
+	dog->name = malloc(sizeof(char) * (len1 + 1));
 	if (dog->name == NULL)
 	{
-			free(dog);
-			return (NULL);
+		free(dog);
+		return (NULL);
 	}
-
-	dog->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
-	if (dog->owner == NULL)
-		{
-			free(dog->name);
-			free(dog);
-			return (NULL);
-		}
-
-	dog->name = _strcopy(dog->name, name);
-	dog->age = age;
-	dog->owner = _strcopy(dog->owner, owner);
-	return (dog);
 }
