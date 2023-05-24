550f540e4c9715a16fac5de5d1dc740c5207106b
MIRAI <mariyamtahri7@gmail.com>
2023-05-24 08:18:47 -0700

    r

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,11 +14,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int j;
 
 	if (array == NULL || size <= 0 || cmp == NULL)
+		return (-1);
 
 	for (j = 0; j < size; j++)
 	{
 		if (cmp(array[j]))
 			return (j);
 	}
-	return (j);
+	return (-1);
 }
