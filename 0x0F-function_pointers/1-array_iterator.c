2bbc74087ed2938540a07a3ee81b9a8a2bc111a6
MIRAI <mariyamtahri7@gmail.com>
2023-05-24 08:04:11 -0700

    s

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,5 +1,4 @@
 #include "function_pointers.h"
-#include <stdio.h>
 
 /**
  * print_name - a function that executes a function given
@@ -11,8 +10,6 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
-		return;
-
+	if (name && f)
 	f(name);
 }
