d04209e757ec7b01d74caa5eec22affb87b2c1c0
MIRAI <mariyamtahri7@gmail.com>
2023-05-24 08:01:34 -0700

    e

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - a function that executes a function given
+ *              as a parameter on each element of an array
+ * @name: string name
+ * @f: pointer to function
+ * Return: void
+ **/
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
+}
