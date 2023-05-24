3e08705913fa6d5d3129670c8a40384ad3437439
MIRAI <mariyamtahri7@gmail.com>
2023-05-24 08:38:34 -0700

    f

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -26,7 +26,7 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	opf = argv[2];
 	n2 = atoi(argv[3]);
 
-	if (get_op_func(op) == NULL || opf[1] != '\0')
+	if (get_op_func(opf) == NULL || opf[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
